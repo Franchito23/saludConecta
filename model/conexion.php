e4257de4b16db16a3be8fd00c507430654328591
Franchito23 <henaosolanof@gmail.com>
2023-06-27 16:07:23 -0500

    dios

--- model/conexion.php
+++ model/conexion.php
@@ -3,16 +3,12 @@
     //CONFIGURACIÓN DE DATOS DE ACCESO  A LA BASE DE DATOS
 
     class Conexion {
-      private $dsn = 'mysql:host=saludconecta.mysql.database.azure.com;port=3306;dbname=BD_saludConecta';
+      private $dsn = 'mysql:host=saludconecta.mysql.database.azure.com;port=33062;dbname=BD_saludConecta';
       private $usuario = 'Frank';
       private $password = 'saludConecta.';
     
       public function conectar() {
           try {
-              $opciones = array(
-                  PDO::MYSQL_ATTR_SSL_CA => '{ruta_al_certificado_CA}',
-                  PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
-              );
               $conn = new PDO($this->dsn, $this->usuario, $this->password, $opciones);
               return $conn;
           } catch (PDOException $e) {
