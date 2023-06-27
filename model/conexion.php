08a9274d5bfd6984381151ff9c647f2098b0c5bb
Franchito23 <henaosolanof@gmail.com>
2023-06-27 17:41:54 -0500

    passyes

--- model/conexion.php
+++ model/conexion.php
@@ -5,7 +5,7 @@
     class Conexion {
       private $dsn = 'mysql:host=saludconecta.mysql.database.azure.com;port=3306;dbname=BD_saludConecta';
       private $usuario = 'Frank';
-      private $password = '';
+      private $password = 'saludConecta.';
     
       public function conectar() {
           try {
