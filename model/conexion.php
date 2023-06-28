e73e2c197a7394dd09381312d84795e58010dc3a
Franchito23 <henaosolanof@gmail.com>
2023-06-27 19:22:32 -0500

    new

--- model/conexion.php
+++ model/conexion.php
@@ -20,39 +20,27 @@
 
 //CONFIGURACIÓN DE DATOS DE ACCESO A LA BASE DE DATOS
 
-class Conexion {
-    private $dsn = 'mysql:host=saludconecta.mysql.database.azure.com;port=3306;dbname=bd_saludconecta';
-    private $usuario = 'Frank';
-    private $password = 'saludConecta.';
-  
-    public function conectar() {
-        try {
-            $con = mysqli_init();
-            mysqli_ssl_set($con, NULL, NULL, "../certificado/DigiCertGlobalRootCA.crt", NULL, NULL);
-            mysqli_real_connect($con, "saludconecta.mysql.database.azure.com", $this->usuario, $this->password, "BD_saludConecta", 3306, MYSQLI_CLIENT_SSL);
-  
-            $conn = new PDO($this->dsn, $this->usuario, $this->password);
-            return $conn;
-        } catch (PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
-            exit;
-        }
-    }
+$ruta_certificado_ca = "{path to CA cert}";
+
+// Crear una instancia de la conexión mysqli
+$conn = mysqli_init();
+
+// Establecer la configuración SSL
+mysqli_ssl_set($conn, NULL, NULL, $ruta_certificado_ca, NULL, NULL);
+
+// Conectar al servidor de base de datos
+mysqli_real_connect($conn, "saludconecta.mysql.database.azure.com", "Frank", "saludConecta.", "BD_saludConecta", 3306, MYSQLI_CLIENT_SSL);
+
+// Verificar si la conexión es exitosa
+if (mysqli_connect_errno()) {
+    echo "Error de conexión: " . mysqli_connect_error();
+    exit;
 }
 
-// Ruta al certificado CA (sustituye con la ubicación correcta)
-$ruta_certificado_ca = "../certificado/DigiCertGlobalRootCA.crt";
-
-// Crear una instancia de la clase de conexión
-$conexion = new Conexion();
-
-// Establecer la ruta al certificado CA en la función mysqli_ssl_set()
-mysqli_ssl_set(null, null, null, $ruta_certificado_ca, null, null);
-
-// Conectar a la base de datos
-$conn = $conexion->conectar();
-
 // Resto del código...
+
+// Cerrar la conexión
+mysqli_close($conn);
 
 
   
