c3a8b36d055f0ed4741bb5b6a0d50c7356a80940
Franchito23 <henaosolanof@gmail.com>
2023-07-07 17:29:11 -0500

    conexion

--- model/conexion.php
+++ model/conexion.php
@@ -3,18 +3,18 @@
     //CONFIGURACIÓN DE DATOS DE ACCESO  A LA BASE DE DATOS
 
     class Conexion {
-      private $dsn = 'mysql:host=localhost;dbname=BD_saludConecta';
-      private $usuario = 'root';
-      private $password = '12345';
-    
-      public function conectar() {
-          try {
-              $conn = new PDO($this->dsn, $this->usuario, $this->password);
-              return $conn;
-          } catch (PDOException $e) {
-              echo "Error de conexión: " . $e->getMessage();
-              exit;
-          }
-      }
-  }
+        private $dsn = 'mysql:host=saludconecta.mysql.database.azure.com;port=3306;dbname=BD_saludConecta';
+        private $usuario = 'Frank';
+        private $password = 'saludConecta.';
+      
+        public function conectar() {
+            try {
+                $conn = new PDO($this->dsn, $this->usuario, $this->password);
+                return $conn;
+            } catch (PDOException $e) {
+                echo "Error de conexión: " . $e->getMessage();
+                exit;
+            }
+        }
+    }
 
